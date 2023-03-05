52b4433da0b422ecb7635925da58b5e795f52859
isayiwantacost <116690661+isayiwantacost@users.noreply.github.com>
2023-03-05 07:57:29 -0600

    Update script1.js

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,1 +1,9 @@
-
+//made by some lazy german guy
+var enhancement1 = (function() {
+  
+  let x, y, z;    //Enhancements variables (BLOCK)
+  //example below
+ x = 10;
+ y = 5;
+ z = 10;
+ let x = y + z;
