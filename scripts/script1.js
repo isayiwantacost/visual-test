fd0712db30377868bdd5b58af9d05d9a43bae7e3
isayiwantacost <116690661+isayiwantacost@users.noreply.github.com>
2023-03-05 08:11:34 -0600

    Update script1.js

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,11 +1,5 @@
 //made by some lazy german guy
 var enhancement1 = (function() {
   
-  let x, y, z;    //Enhancements variables (BLOCK)
-  //example below
- x = 10;
- y = 5;
- z = 10;
- let x = y + z;
   //i really dont know why i wrote the above cuz its useless
   console.log("your probably here if your bored.");
