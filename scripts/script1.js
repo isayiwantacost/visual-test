b7d4c6773f4aed991affae470392177e74b03cd2
isayiwantacost <116690661+isayiwantacost@users.noreply.github.com>
2023-03-06 16:24:51 -0600

    Update script1.js

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,5 +1,6 @@
-//made by some lazy german guy
-var enhancement1 = (function() {
-  
-  //i really dont know why i wrote the above cuz its useless
+
   console.log("your probably here if your bored.");
+ console.log("your probably here if your bored.");
+ console.log("your probably here if your bored.");
+ console.log("your probably here if your bored.");
+
